af1f55cee29d3084e395f2ebd43e2e857053e281
eys <75747082+eyyss@users.noreply.github.com>
2023-08-12 17:08:59 +0300

    aaa

---
+++ Assets/_Game/Scripts/Mission.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    public float timer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            StartCoroutine(IncreaseTimer());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+            timer = 0;
+        }
+    }
+    IEnumerator IncreaseTimer()
+    {
+        yield return new WaitForSeconds(1f);
+        timer += 1;
+        if (timer > 5)
+        {
+            GameEvents.missionComplateEvent?.Invoke();
+        }
+        else StartCoroutine(IncreaseTimer());
+    }
+}
---
+++ Assets/_Game/Scripts/MissionManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class MissionManager : MonoBehaviour
+{
+    public List<Transform> missionTransforms;
+
+    public Transform currentMissionTransform;
+    private Transform lastSelectedMission; // Son seçilen görevi tutan değişken.
+
+    [Header("Arrow")]
+    public GameObject Arrow;
+    public void Start()
+    {
+        FindMission();
+    }
+
+    public void FindMission()
+    {
+        do
+        {
+            int randomMissionIndex = Random.Range(0, missionTransforms.Count);
+            currentMissionTransform = missionTransforms[randomMissionIndex];
+        }
+        while (currentMissionTransform == lastSelectedMission); // Eğer yeni görev, son seçilen görevle aynı ise tekrar seçim yap.
+
+        lastSelectedMission = currentMissionTransform;
+        currentMissionTransform.gameObject.SetActive(true);
+
+        //int randomMissionIndex = Random.Range(0, missionTransforms.Count);
+        //currentMissionTransform = missionTransforms[randomMissionIndex];
+        //currentMissionTransform.gameObject.SetActive(true);
+    }
+    void LateUpdate()
+    {
+        if (currentMissionTransform != null)
+        {
+            Arrow.SetActive(true);
+            Arrow.transform.LookAt(currentMissionTransform.position);
+        }
+        else Arrow.SetActive(false);
+    }
+    void OnEnable()
+    {
+        GameEvents.missionComplateEvent.AddListener(MissionComplateEvent);
+    }
+
+
+    void OnDisable()
+    {
+        GameEvents.missionComplateEvent.RemoveListener(MissionComplateEvent);
+    }
+
+    private void MissionComplateEvent()
+    {
+        FindMission();
+    }
+}
